2133a3afc01039c288e845f1f49f7366434efb41
TTHHR <harrytit@foxmail.com>
2017-08-05 19:55:16 +0800

    new project

---
+++ app/src/main/java/cn/qingyuyu/c4droidcode/SplashActivity.kt
@@ -0,0 +1,12 @@
+package cn.qingyuyu.c4droidcode
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
