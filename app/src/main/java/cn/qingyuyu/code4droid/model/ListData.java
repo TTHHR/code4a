162ea3819d79fa921559101646e91dbcba88c06c
DXkite <dxkite@163.com>
2017-11-04 20:12:20 +0800

    测试：调用文章接口

--- app/src/main/java/cn/qingyuyu/code4droid/model/ListData.java
+++ app/src/main/java/cn/qingyuyu/code4droid/model/ListData.java
@@ -1,6 +1,10 @@
 package cn.qingyuyu.code4droid.model;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+
+import cn.qingyuyu.code4droid.remote.bean.Article;
 
 /**
  * Created by harrytit on 2017/10/20.
@@ -25,4 +29,13 @@ public class ListData {
        listData.add("测试数据");
        listData.add("测试数据");
    }
+
+    public void setArticles(@NotNull ArrayList<Article> articleList) {
+        listData.clear();
+        listData.add("<--- 测试数据 --->");
+        for (Article article: articleList){
+            listData.add(article.getTitle());
+        }
+        listData.add("<--- 测试数据 --->");
+    }
 }
