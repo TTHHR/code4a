4e3975c4d7f9e6ca03a23aa7fed02d76386297ee
DXkite <dxkite@163.com>
2017-11-05 22:38:01 +0800

    测试：用户登陆 异常：onResume调用刷新延迟

--- app/src/main/java/cn/qingyuyu/code4a/model/User.java
+++ app/src/main/java/cn/qingyuyu/code4a/model/User.java
@@ -6,8 +6,6 @@ import android.util.Log;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 
 import cn.qingyuyu.code4a.remote.Remote;
 import cn.qingyuyu.commom.SomeValue;
@@ -18,57 +16,63 @@ import cn.qingyuyu.commom.service.FileDealService;
  */
 
 public class User {
-    private static   String iconName = null;
+    private static String iconName = null;
     private static String userName = null;
 
-    private static User u=new User();
+    private static User u = new User();
+    private static long lastModify=0;
 
-
-    private User()
-    {
+    private User() {
 
     }
 
 
-    public static User getInstance()
-    {
-      if (iconName == null && userName == null) {
-          try{
-                String data=new FileDealService().readFile(SomeValue.dirPath+SomeValue.userData);
-              JSONObject user = JSONObject.parseObject(data);
-                  iconName = user.getString("portrait");
-                   userName = user.getString("uname");
-               } catch ( Exception e) {
-                   Log.e("User", "not login");
-               }
-
-          }
+    public static User getInstance() {
+        String userData=SomeValue.dirPath + SomeValue.userData;
+        boolean firstRun=iconName == null && userName == null;
+        boolean modified=false;
+        File userFile=new File(userData);
+        if (userFile.exists()){
+            modified=userFile.lastModified() > lastModify;
+            lastModify=new File(userData).lastModified();
+        }
+        if (firstRun || modified) {
+            try {
+                String data = new FileDealService().readFile(userData);
+                JSONObject user = JSONObject.parseObject(data);
+                iconName = user.getString("portrait");
+                userName = user.getString("uname");
+            } catch (Exception e) {
+                Log.e("User", "not login");
+            }
+        }
         return u;
     }
 
 
-    public boolean isLogind(){
+    public boolean isLogind() {
 
         return userName != null;
 
 
     }
-    public boolean logout(){
-        try {
-            userName=null;
 
-            new FileDealService().delFile(SomeValue.dirPath+SomeValue.userData);//删除本地信息
+    public boolean logout() {
+        try {
+            userName = null;
+
+            new FileDealService().delFile(SomeValue.dirPath + SomeValue.userData);//删除本地信息
             Remote.user.method("signout").call();//服务器退出登陆
             return true;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return false;
         }
     }
-    public String getUserName(){
+
+    public String getUserName() {
         return userName;
     }
+
     public Uri getimgUri() {
         File file = new File(SomeValue.dirPath + iconName);
         return Uri.fromFile(file);
