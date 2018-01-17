e02503634c73eaf249b1d42f8185d1682bf7088c
e <harrytit@foxmail.com>
2018-01-17 17:47:28 +0800

    <新增>创建和编辑文章

---
+++ app/src/main/java/cn/atd3/code4a/library/fileselect/constant/Constant.java
@@ -0,0 +1,17 @@
+package cn.atd3.code4a.library.fileselect.constant;
+
+/**
+ * Created by admin on 2016/11/23.
+ */
+public class Constant {
+
+    public static final String DATA_TYPE_KEY = "data_type_key";
+    public static final String CUR_APP_DB_PATH = "curAppDbPath";
+    public static final String CUR_APP_TMP_DB_PATH = "curAppTmpDbPath";
+    public static final String FRAGMENT_AGR_KEY = "fragment_key";
+    public static final String FILE_DATA_TYPE = "file_data_type";
+    public static final String CLICK_POSITION = "click_position";
+    public static final String BACK_PATH = "back_path";
+    public static final String GOTO_PATH = "goto_path";
+
+}
