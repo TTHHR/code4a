afb865bd3221438352182cbb7c8deb6071fdc7f2
TTHHR <harrytit@foxmail.com>
2018-01-28 18:31:25 +0800

    <新增>构建错误

--- app/src/main/java/cn/atd3/code4a/presenter/MainPresenter.java
+++ app/src/main/java/cn/atd3/code4a/presenter/MainPresenter.java
@@ -1,15 +1,14 @@
 package cn.atd3.code4a.presenter;
 
+import android.content.Context;
 import android.os.Build;
+import android.util.Log;
 
-import java.io.IOException;
-
-import cn.atd3.code4a.model.model.MessageModel;
+import cn.atd3.code4a.Constant;
 import cn.atd3.code4a.net.Remote;
 import cn.atd3.code4a.view.inter.MainViewInterface;
-import cn.atd3.code4a.view.view.SplashActivity;
-import cn.atd3.proxy.exception.PermissionException;
-import cn.atd3.proxy.exception.ServerException;
+import cn.dxkite.common.ui.notification.popbanner.Adapter;
+import cn.dxkite.common.ui.notification.popbanner.Information;
 
 /**
  * Created by harry on 2018/1/13.
@@ -17,15 +16,52 @@ import cn.atd3.proxy.exception.ServerException;
 
 public class MainPresenter {
     MainViewInterface mvi;
-    MessageModel mm;
     public MainPresenter(MainViewInterface mvi)
     {
         this.mvi=mvi;
-        mm=new MessageModel();
     }
-
-    private void showMessageBanner()
+    public void showMessageBanner()
     {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final Object msg = Remote.androidMessage.method("pull", Information.class).call();
+                    if (msg instanceof Information){
+                        Adapter a=new Adapter() {
+                            @Override
+                            public Information refresh() {
+                                return (Information) msg;
+                            }
+                        };
+                       mvi.showMessageBanner(a);
+                    }
+                } catch (Exception e) {
+                    Log.e("get message",""+e);
+                }
+            }
+        }).start();
 
+    }
+    public void collection(Context c)
+    {
+        if(!Constant.collectioninfo)
+            return;
+        final String deviceId = Constant.getUuid();
+        final String deviceName = Build.MODEL;
+        final String packageName =c.getPackageName();
+        final String activityName = "MainActivity";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+               // name = "collectingInfo"
+                try {
+                    // TDDO: 获取驱动ID
+                    Remote.collection.method("android").call(deviceName, deviceId, packageName, activityName);
+                } catch (Exception e) {
+                    Log.e("collection",""+e);
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/cn/atd3/code4a/view/inter/MainViewInterface.kt
+++ app/src/main/java/cn/atd3/code4a/view/inter/MainViewInterface.kt
@@ -1,15 +1,13 @@
 package cn.atd3.code4a.view.inter
 
-import cn.atd3.code4a.model.inter.MessageModelInterface
+import cn.dxkite.common.ui.notification.popbanner.Adapter
+
 
 /**
  * Created by harry on 2018/1/13.
  */
 interface MainViewInterface {
 
-    fun showMessageBanner(text : MessageModelInterface)
-
-
-
+    fun showMessageBanner(a: Adapter)
 
 }
