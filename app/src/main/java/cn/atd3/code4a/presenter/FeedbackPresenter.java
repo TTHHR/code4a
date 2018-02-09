ef6b406eb85cdccd9f2fb89f707e34ab5ec36041
TTHHR <harrytit@foxmail.com>
2018-02-09 18:18:23 +0800

    <新增>反馈动作

---
+++ app/src/main/java/cn/atd3/code4a/presenter/FeedbackPresenter.java
@@ -0,0 +1,15 @@
+package cn.atd3.code4a.presenter;
+
+import cn.atd3.code4a.view.inter.FeedbackInterface;
+
+public class FeedbackPresenter {
+    private FeedbackInterface fbi;
+    public FeedbackPresenter(FeedbackInterface fbi)
+    {
+        this.fbi=fbi;
+    }
+    public void upToServer(String text)
+    {
+
+    }
+}
