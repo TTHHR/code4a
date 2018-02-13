1f2ff7d08adf88fd243c9d87d7bd15bf3b9a0520
TTHHR <harrytit@foxmail.com>
2018-02-13 10:13:21 +0800

    <修复>发布代码头文件丢失

---
+++ app/src/main/java/cn/atd3/code4a/util/ReplaceCHeadInHtml.java
@@ -0,0 +1,36 @@
+package cn.atd3.code4a.util;
+/*
+
+仅仅是用来替换c语言的头文件
+
+create:harry 2018/2/13
+
+ */
+public class ReplaceCHeadInHtml {
+	private static ReplaceCHeadInHtml rchih=null;
+	private ReplaceCHeadInHtml() {}
+	public static ReplaceCHeadInHtml getIns() {
+		if(rchih==null)
+			rchih=new ReplaceCHeadInHtml();
+		return rchih;
+	}
+	
+	public String getHtml(String html)
+	{
+		
+		return findAndReplace(html);
+	}
+	private String findAndReplace(String html)
+	{
+		
+		for(int i=-1; i<=html.lastIndexOf("#include");++i)
+		{
+		     i=html.indexOf("#include",i);
+		     int j=html.indexOf("<br>",i);
+		     String findString=html.substring(i, j);
+		     System.out.println("find head"+findString);
+		     html=html.replace(findString, findString.replace("<", "&lt;").replace(">", "&gt;"));
+		} 
+		return html;
+	}
+}
