15b43a356d848f5dd1c33c0f32042ae97fadf20d
e <harrytit@foxmail.com>
2018-03-25 17:24:55 +0800

    <修改>增加用户更新

--- app/src/main/java/cn/atd3/code4a/presenter/AboutPresenter.java
+++ app/src/main/java/cn/atd3/code4a/presenter/AboutPresenter.java
@@ -1,7 +1,12 @@
 package cn.atd3.code4a.presenter;
 
-import android.content.Intent;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.atd3.code4a.model.model.UpdateInfo;
+import cn.atd3.code4a.net.Remote;
 import cn.atd3.code4a.view.inter.AboutInterface;
 
 public class AboutPresenter {
@@ -10,8 +15,36 @@ public class AboutPresenter {
         {
             this.abi=abi;
         }
-    public void onCheakUpdate()
+    public void onCheakUpdate(final Context con)
     {
-
+        abi.showWaitDialog();
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Object a = Remote.androidMessage.method("update", UpdateInfo.class).call();
+                            if(a instanceof UpdateInfo)
+                            {
+                                String versionName = con.getPackageManager().getPackageInfo(
+                                        con.getPackageName(), 0).versionName;
+                                if(versionName.equals(((UpdateInfo) a).getVersion()))
+                                {
+                                    abi.dismissWaitDialog();
+                                }
+                                else {
+                                    abi.dismissWaitDialog();
+                                    Log.e("found new",""+((UpdateInfo) a).getVersion());
+                                    abi.showUpdateInfo((UpdateInfo) a);
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            abi.dismissWaitDialog();
+                        }
+                    }
+                }
+        ).start();
     }
 }
